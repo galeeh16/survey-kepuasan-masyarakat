c17acfbaa6e965a7413cf5e06f7c9cc6fed759fd
Galih Anggoro Jati <anggorogalih8@gmail.com>
2022-10-08 17:02:47 +0700

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,7 +10,7 @@ final class DashboardController extends Controller
 {
     public function index()
     {
-        $data_dashboard = Cache::remember('data_dashboard', $ttl=300, function () {
+        // $data_dashboard = Cache::remember('data_dashboard', $ttl=300, function () {
             $total_ak1 = DB::table('tbl_responden')->where('id_layanan', 1)->count();
             $total_rekom_passport = DB::table('tbl_responden')->where('id_layanan', 2)->count();
             $total_pelatihan = DB::table('tbl_responden')->where('id_layanan', 3)->count();
@@ -35,7 +35,9 @@ final class DashboardController extends Controller
                     ])
                     ->count();
 
-            return [
+            $total_mengikuti_survey = $total_ak1 + $total_rekom_passport + $total_pelatihan + $total_lpk + $total_perusahaan + $total_hub_intl;
+
+            $data_dashboard = [
                 'show_logo' => 'show',
                 'total_ak1' => number_format($total_ak1),
                 'total_rekom_passport' => number_format($total_rekom_passport),
@@ -45,75 +47,37 @@ final class DashboardController extends Controller
                 'total_hub_intl' => number_format($total_hub_intl),
                 'total_bulan_ini' => number_format($total_bulan_ini),
                 'total_bulan_sebelumnya' => number_format($total_bulan_sebelumnya),
+                'total_mengikuti_survey' => number_format($total_mengikuti_survey)
             ];
-        });
+        // });
 
         return view('dashboard', $data_dashboard);
     }
 
     public function dataGrafikBar(): JsonResponse
     {
-        $data_grafik = Cache::remember('data_dashboard_grafik_bar', $ttl=300, function () {
-            $total_ak1 = DB::table('tbl_responden')->where('id_layanan', 1)->count();
-            $total_rekom_passport = DB::table('tbl_responden')->where('id_layanan', 2)->count();
-            $total_pelatihan = DB::table('tbl_responden')->where('id_layanan', 3)->count();
-            $total_lpk = DB::table('tbl_responden')->where('id_layanan', 4)->count();
-            $total_perusahaan = DB::table('tbl_responden')->where('id_layanan', 5)->count();
-            $total_hub_intl = DB::table('tbl_responden')->where('id_layanan', 6)->count();
+        // $data_grafik = Cache::remember('data_dashboard_grafik_bar', $ttl=300, function () {
+            $tgl_awal_bulan_ini = date('Y-m') . '-01'; 
+            $tgl_now = date('Y-m-d');
 
-            $bln_sebelum_1 = date('m', strtotime('now -1 month'));
-            $thn_sebelum_1 = date('Y', strtotime('now -1 month'));
-            $total_day_before = cal_days_in_month(CAL_GREGORIAN, date('m', strtotime('now -1 month')), date('Y', strtotime('now -1 month')));
-
-            $total_bulan_sebelumnya = DB::table('tbl_responden')
-                        ->whereBetween('created_at', [
-                            $thn_sebelum_1 . '-'. $bln_sebelum_1 . '-' . '01', 
-                            $thn_sebelum_1 . '-'. $bln_sebelum_1 . '-' .  $total_day_before])
-                        ->count();
-                        
-            $total_bulan_ini = DB::table('tbl_responden')->where('created_at', date('Y-m-d'))->count();
-
-            // [
-            //     {
-            //         name: 'AK1',
-            //         data: [0, 0, 10000]
-            //     }, 
-            //     {
-            //         name: 'Rekom Passport',
-            //         data: [0, 0, 1100]
-            //     },
-            //     {
-            //         name: 'Pelatihan',
-            //         data: [0, 0, 90]
-            //     },
-            //     {
-            //         name: 'LPK',
-            //         data: [0, 0, 900]
-            //     },
-            //     {
-            //         name: 'Pencatatan Perusahaan',
-            //         data: [0, 0, 290]
-            //     },
-            //     {
-            //         name: 'Perselisihan Hubungan Industrial',
-            //         data: [0, 0, 100]
-            //     }
-            // ]
+            $total_ak1 = DB::table('tbl_responden')->where('id_layanan', 1)->whereBetween('created_at', [$tgl_awal_bulan_ini, $tgl_now])->count();
+            $total_rekom_passport = DB::table('tbl_responden')->where('id_layanan', 2)->whereBetween('created_at', [$tgl_awal_bulan_ini, $tgl_now])->count();
+            $total_pelatihan = DB::table('tbl_responden')->where('id_layanan', 3)->whereBetween('created_at', [$tgl_awal_bulan_ini, $tgl_now])->count();
+            $total_lpk = DB::table('tbl_responden')->where('id_layanan', 4)->whereBetween('created_at', [$tgl_awal_bulan_ini, $tgl_now])->count();
+            $total_perusahaan = DB::table('tbl_responden')->where('id_layanan', 5)->whereBetween('created_at', [$tgl_awal_bulan_ini, $tgl_now])->count();
+            $total_hub_intl = DB::table('tbl_responden')->where('id_layanan', 6)->whereBetween('created_at', [$tgl_awal_bulan_ini, $tgl_now])->count();
 
             $data_series = [
-
+                [
+                    'name' => 'Layanan',
+                    'data' => [$total_ak1, $total_rekom_passport, $total_pelatihan, $total_lpk, $total_perusahaan, $total_hub_intl]
+                ]
             ];
+            
+        //     return $data_series;
+        // });
 
-            return [
-                'total_ak1' => number_format($total_ak1),
-                'total_rekom_passport' => number_format($total_rekom_passport),
-                'total_pelatihan' => number_format($total_pelatihan),
-                'total_lpk' => number_format($total_lpk),
-                'total_perusahaan' => number_format($total_perusahaan),
-                'total_hub_intl' => number_format($total_hub_intl),
-            ];
-        });
-
-        return response()->json($data_grafik);
+        // return response()->json($data_grafik);
+        return response()->json($data_series);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -121,21 +121,23 @@
         <div class="col-lg-12 col-sm-12">
             <div class="card">
                 <div class="card-body">
-                    <div class="d-flex justify-content-end w-100 mb-5">
-                        <div class="me-4 px-3 py-2 rounded" style="width: 205px; background-color: #d8ddfa;">
-                            <h5 class="mb-0 fw-bold mb-1" style="font-size: 18px;">Bulan Ini :</h5>
-                            <h4 class="mb-0 fw-bold text-muted">{{ $total_bulan_ini }}</h4>
+                    <div class="d-flex justify-content-between w-100 mb-5">
+                        <div>
+                            <h5 class="mb-0 fw-bold mb-1" style="font-size: 18px;">Total Mengikuti Survey</h5>
+                            <h4 class="fw-bold text-muted">{{ $total_mengikuti_survey }}</h4>
                         </div>
-                        <div class="px-3 py-2 rounded" style="width: 205px; background-color: #d8ddfa;">
-                            <h5 class="mb-0 fw-bold mb-1" style="font-size: 18px;">Bulan Sebelumnya :</h5>
-                            <h4 class="mb-0 fw-bold text-muted">{{ $total_bulan_sebelumnya }}</h4>
-                        </div>
+                       <div class="d-flex">
+                            <div class="me-4 px-3 py-2 rounded" style="width: 205px; background-color: #d8ddfa;">
+                                <h5 class="mb-0 fw-bold mb-1" style="font-size: 18px;">Bulan Ini :</h5>
+                                <h4 class="mb-0 fw-bold text-muted">{{ $total_bulan_ini }}</h4>
+                            </div>
+                            <div class="px-3 py-2 rounded" style="width: 205px; background-color: #d8ddfa;">
+                                <h5 class="mb-0 fw-bold mb-1" style="font-size: 18px;">Bulan Sebelumnya :</h5>
+                                <h4 class="mb-0 fw-bold text-muted">{{ $total_bulan_sebelumnya }}</h4>
+                            </div>
+                       </div>
                     </div>
                     <div class="d-flex justify-content-between">
-                        <div>
-                            <h4 class="fw-bold text-muted">12.480</h4>
-                            <div class="text-muted">Total Mengikuti Survey</div>
-                        </div>
                         <div class="text-muted">
                             {{ date('d F Y') }}
                         </div>
@@ -149,13 +151,13 @@
         </div>
     </div>
 
-    <div class="row mt-4 mb-5" id="layanan">
+    <div class="row mt-5 mb-5" id="layanan">
         <div class="col-12 mb-4">
             <h2 class="text-center">Layanan</h2>
             <p class="text-center">Pilih layanan dibawah ini</p>
         </div>
 
-        <div class="col-xl-3 col-lg-3 col-md-6 col-xs-12">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-xs-12">
             <a href="{{ url('/ak1') }}">
                 <div class="card card-body">
                     <div class="text-center">
@@ -164,14 +166,14 @@
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M8.07996 6.6499V6.6599C7.64896 6.6599 7.29996 7.0099 7.29996 7.4399C7.29996 7.8699 7.64896 8.2199 8.07996 8.2199H11.069C11.5 8.2199 11.85 7.8699 11.85 7.4289C11.85 6.9999 11.5 6.6499 11.069 6.6499H8.07996ZM15.92 12.7399H8.07996C7.64896 12.7399 7.29996 12.3899 7.29996 11.9599C7.29996 11.5299 7.64896 11.1789 8.07996 11.1789H15.92C16.35 11.1789 16.7 11.5299 16.7 11.9599C16.7 12.3899 16.35 12.7399 15.92 12.7399ZM15.92 17.3099H8.07996C7.77996 17.3499 7.48996 17.1999 7.32996 16.9499C7.16996 16.6899 7.16996 16.3599 7.32996 16.1099C7.48996 15.8499 7.77996 15.7099 8.07996 15.7399H15.92C16.319 15.7799 16.62 16.1199 16.62 16.5299C16.62 16.9289 16.319 17.2699 15.92 17.3099Z" fill="currentColor"></path>                                
                         </svg>                            
                     </div>
-                    <div class="fw-bold fs-4 text-center">
+                    <div class="fw-bold fs-5 mt-2 text-center">
                         AK1
                     </div>
                 </div>
             </a>
         </div>
-        <div class="col-xl-3 col-lg-3 col-md-6 col-xs-12">
-            <a href="{{ url('/ak1') }}">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-xs-12">
+            <a href="{{ url('/rekom-passport') }}">
                 <div class="card card-body">
                     <div class="text-center">
                         <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                                
@@ -179,14 +181,14 @@
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M8.07996 6.6499V6.6599C7.64896 6.6599 7.29996 7.0099 7.29996 7.4399C7.29996 7.8699 7.64896 8.2199 8.07996 8.2199H11.069C11.5 8.2199 11.85 7.8699 11.85 7.4289C11.85 6.9999 11.5 6.6499 11.069 6.6499H8.07996ZM15.92 12.7399H8.07996C7.64896 12.7399 7.29996 12.3899 7.29996 11.9599C7.29996 11.5299 7.64896 11.1789 8.07996 11.1789H15.92C16.35 11.1789 16.7 11.5299 16.7 11.9599C16.7 12.3899 16.35 12.7399 15.92 12.7399ZM15.92 17.3099H8.07996C7.77996 17.3499 7.48996 17.1999 7.32996 16.9499C7.16996 16.6899 7.16996 16.3599 7.32996 16.1099C7.48996 15.8499 7.77996 15.7099 8.07996 15.7399H15.92C16.319 15.7799 16.62 16.1199 16.62 16.5299C16.62 16.9289 16.319 17.2699 15.92 17.3099Z" fill="currentColor"></path>                                
                         </svg>                            
                     </div>
-                    <div class="fw-bold fs-4 text-center">
-                        AK1
+                    <div class="fw-bold fs-5 mt-2 text-center">
+                        Rekom Passport
                     </div>
                 </div>
             </a>
         </div>
-        <div class="col-xl-3 col-lg-3 col-md-6 col-xs-12">
-            <a href="{{ url('/ak1') }}">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-xs-12">
+            <a href="{{ url('/pelatihan') }}">
                 <div class="card card-body">
                     <div class="text-center">
                         <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                                
@@ -194,14 +196,14 @@
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M8.07996 6.6499V6.6599C7.64896 6.6599 7.29996 7.0099 7.29996 7.4399C7.29996 7.8699 7.64896 8.2199 8.07996 8.2199H11.069C11.5 8.2199 11.85 7.8699 11.85 7.4289C11.85 6.9999 11.5 6.6499 11.069 6.6499H8.07996ZM15.92 12.7399H8.07996C7.64896 12.7399 7.29996 12.3899 7.29996 11.9599C7.29996 11.5299 7.64896 11.1789 8.07996 11.1789H15.92C16.35 11.1789 16.7 11.5299 16.7 11.9599C16.7 12.3899 16.35 12.7399 15.92 12.7399ZM15.92 17.3099H8.07996C7.77996 17.3499 7.48996 17.1999 7.32996 16.9499C7.16996 16.6899 7.16996 16.3599 7.32996 16.1099C7.48996 15.8499 7.77996 15.7099 8.07996 15.7399H15.92C16.319 15.7799 16.62 16.1199 16.62 16.5299C16.62 16.9289 16.319 17.2699 15.92 17.3099Z" fill="currentColor"></path>                                
                         </svg>                            
                     </div>
-                    <div class="fw-bold fs-4 text-center">
-                        AK1
+                    <div class="fw-bold fs-5 mt-2 text-center">
+                        Pelatihan
                     </div>
                 </div>
             </a>
         </div>
-        <div class="col-xl-3 col-lg-3 col-md-6 col-xs-12">
-            <a href="{{ url('/ak1') }}">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-xs-12">
+            <a href="{{ url('/lpk') }}">
                 <div class="card card-body">
                     <div class="text-center">
                         <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                                
@@ -209,23 +211,63 @@
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M8.07996 6.6499V6.6599C7.64896 6.6599 7.29996 7.0099 7.29996 7.4399C7.29996 7.8699 7.64896 8.2199 8.07996 8.2199H11.069C11.5 8.2199 11.85 7.8699 11.85 7.4289C11.85 6.9999 11.5 6.6499 11.069 6.6499H8.07996ZM15.92 12.7399H8.07996C7.64896 12.7399 7.29996 12.3899 7.29996 11.9599C7.29996 11.5299 7.64896 11.1789 8.07996 11.1789H15.92C16.35 11.1789 16.7 11.5299 16.7 11.9599C16.7 12.3899 16.35 12.7399 15.92 12.7399ZM15.92 17.3099H8.07996C7.77996 17.3499 7.48996 17.1999 7.32996 16.9499C7.16996 16.6899 7.16996 16.3599 7.32996 16.1099C7.48996 15.8499 7.77996 15.7099 8.07996 15.7399H15.92C16.319 15.7799 16.62 16.1199 16.62 16.5299C16.62 16.9289 16.319 17.2699 15.92 17.3099Z" fill="currentColor"></path>                                
                         </svg>                            
                     </div>
-                    <div class="fw-bold fs-4 text-center">
-                        AK1
+                    <div class="fw-bold fs-5 mt-2 text-center">
+                        LPK
                     </div>
                 </div>
             </a>
         </div>
+        <div class="col-xl-4 col-lg-4 col-md-6 col-xs-12">
+            <a href="{{ url('/pencatatan-perusahaan') }}">
+                <div class="card card-body">
+                    <div class="text-center">
+                        <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                                
+                            <path opacity="0.4" d="M16.191 2H7.81C4.77 2 3 3.78 3 6.83V17.16C3 20.26 4.77 22 7.81 22H16.191C19.28 22 21 20.26 21 17.16V6.83C21 3.78 19.28 2 16.191 2Z" fill="currentColor"></path>                                
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.07996 6.6499V6.6599C7.64896 6.6599 7.29996 7.0099 7.29996 7.4399C7.29996 7.8699 7.64896 8.2199 8.07996 8.2199H11.069C11.5 8.2199 11.85 7.8699 11.85 7.4289C11.85 6.9999 11.5 6.6499 11.069 6.6499H8.07996ZM15.92 12.7399H8.07996C7.64896 12.7399 7.29996 12.3899 7.29996 11.9599C7.29996 11.5299 7.64896 11.1789 8.07996 11.1789H15.92C16.35 11.1789 16.7 11.5299 16.7 11.9599C16.7 12.3899 16.35 12.7399 15.92 12.7399ZM15.92 17.3099H8.07996C7.77996 17.3499 7.48996 17.1999 7.32996 16.9499C7.16996 16.6899 7.16996 16.3599 7.32996 16.1099C7.48996 15.8499 7.77996 15.7099 8.07996 15.7399H15.92C16.319 15.7799 16.62 16.1199 16.62 16.5299C16.62 16.9289 16.319 17.2699 15.92 17.3099Z" fill="currentColor"></path>                                
+                        </svg>                            
+                    </div>
+                    <div class="fw-bold fs-5 mt-2 text-center">
+                        Pencatatan Perusahaan
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-6 col-xs-12">
+            <a href="{{ url('/perselisihan-hubungan-industrial') }}">
+                <div class="card card-body">
+                    <div class="text-center">
+                        <svg width="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">                                
+                            <path opacity="0.4" d="M16.191 2H7.81C4.77 2 3 3.78 3 6.83V17.16C3 20.26 4.77 22 7.81 22H16.191C19.28 22 21 20.26 21 17.16V6.83C21 3.78 19.28 2 16.191 2Z" fill="currentColor"></path>                                
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.07996 6.6499V6.6599C7.64896 6.6599 7.29996 7.0099 7.29996 7.4399C7.29996 7.8699 7.64896 8.2199 8.07996 8.2199H11.069C11.5 8.2199 11.85 7.8699 11.85 7.4289C11.85 6.9999 11.5 6.6499 11.069 6.6499H8.07996ZM15.92 12.7399H8.07996C7.64896 12.7399 7.29996 12.3899 7.29996 11.9599C7.29996 11.5299 7.64896 11.1789 8.07996 11.1789H15.92C16.35 11.1789 16.7 11.5299 16.7 11.9599C16.7 12.3899 16.35 12.7399 15.92 12.7399ZM15.92 17.3099H8.07996C7.77996 17.3499 7.48996 17.1999 7.32996 16.9499C7.16996 16.6899 7.16996 16.3599 7.32996 16.1099C7.48996 15.8499 7.77996 15.7099 8.07996 15.7399H15.92C16.319 15.7799 16.62 16.1199 16.62 16.5299C16.62 16.9289 16.319 17.2699 15.92 17.3099Z" fill="currentColor"></path>                                
+                        </svg>                            
+                    </div>
+                    <div class="fw-bold fs-5 mt-2 text-center">
+                        Perselisihan Hubungan Industrial
+                    </div>
+                </div>
+            </a>
+        </div>
+       
     </div>
 @endsection
 
 @section('script')
 <script>
-$(document).ready(function() {
-    var options = {
-        series: [],
+function rupiah(number) {
+    const formatNumbering = new Intl.NumberFormat("id-ID", {minimumFractionDigits: 0});
+    return formatNumbering.format(number)
+}
+
+var options = {
+        series:  [
+                {
+                    name: 'Layanan',
+                    data: [0,0,0,0,0,0]
+                }, 
+            ],
         chart: {
             height: 350,
-            type: 'area',
+            type: 'bar',
             toolbar: {
                 show: false
             }
@@ -236,7 +278,7 @@ $(document).ready(function() {
         },
         xaxis: {
             show: false,
-            categories: ["Jul", "Aug", "Sep"]
+            categories: ["AK1", "Rekom Passport", "Pelatihan", "LPK", "Pencatatan Perusahaan", "Perselisihan Hub Industrial"]
         },
         tooltip: {
             x: {
@@ -251,8 +293,28 @@ $(document).ready(function() {
                 opacityTo: 0.1
             }
         },
+        plotOptions: {
+            bar: {
+                distributed: false, // different color each bar
+                columnWidth: '60%',
+                borderRadius: 0,
+                dataLabels: {
+                    orientation: 'horizontal',
+                    position: 'top',
+                    minItem: 0,
+                    hideOverflowingLabels: true,
+                }
+            },
+        },
         dataLabels: {
-            enabled: false,
+            offsetY: -18,
+            style: {
+            colors: ['#222'],
+            fontWeight: 'normal'
+            },
+            formatter: function (val, opts) {
+                return rupiah(val);
+            },
         },
         legend: {
             position: 'top',
@@ -265,7 +327,21 @@ $(document).ready(function() {
     };
 
     var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+function fetchDataGraphic() {
+    fetch(`/data-grafik-bar`)
+        .then(response => response.json())
+        .then(json => {
+            chart.updateSeries(json);
+        });
+}
+
+$(document).ready(function() {
     chart.render();
+
+    fetchDataGraphic();
 });
+
+
 </script>
 @endsection
