595755f59eac5ac5f4367f3cf33b08e1b80a2125
Galih Anggoro Jati <anggorogalih8@gmail.com>
2022-10-09 20:01:29 +0700

    add dashboard logo

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -5,9 +5,9 @@
 @endsection
 
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <h2>Welcome Admin</h2>
-        </div>
+    <h2>Welcome Admin</h2>
+
+    <div class="d-flex justify-content-center mt-5">
+        <img src="{{ asset('assets/images/dashboard.svg') }}" style="max-width: 650px;">
     </div>
 @endsection
